46aec9d39c1f1e4ff5d9282eb953684a00f14b63
Tyler B <tahbristol@gmail.com>
2017-09-24 01:14:27 -0400

    added user valiations for email, added errors to view for user#new

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,13 @@ class UsersController < ApplicationController
 
   def create
     @user = User.create(user_params)
+    if @user.save
+    @user = User.create(user_params)
     redirect_to user_path(@user)
+    else
+
+      render :new
+    end
   end
 
   def show
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
+	validates :email, presence: true
 	has_many :job_users
 	has_many :jobs, through: :job_users
   has_secure_password
-  
+
 end
