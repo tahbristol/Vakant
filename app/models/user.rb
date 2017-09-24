33b8cb7efe9f11f78764f9acd73dde2055c52d7f
Tyler B <tahbristol@gmail.com>
2017-09-24 01:16:34 -0400

    added password length validation to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
 	validates :email, presence: true
+	validates :password, length: { in: 6..10}
 	has_many :job_users
 	has_many :jobs, through: :job_users
   has_secure_password
