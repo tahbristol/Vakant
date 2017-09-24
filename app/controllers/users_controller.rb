f93327a2f7cf11055b2827137d480018c583f1f9
Tyler B <tahbristol@gmail.com>
2017-09-24 00:53:17 -0400

    added user controller and signup route with new and show views

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < ApplicationController
+
+
+  def index
+
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    redirect_to user_path(@user)
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destory
+
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:email, :password)
+    end
+
+end
