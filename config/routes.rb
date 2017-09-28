353bc5fc39c0ca83c5c0e9c3d202d53475c14a8e
Tyler B <tahbristol@gmail.com>
2017-09-28 06:21:19 -0400

    set up devise organization, trying to make nav links dependant on login type user vs org

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {sessions: 'users/sessions'}
+  devise_for :users, path: 'users', controllers: {sessions: 'users/sessions'}
+  devise_for :organizations, path: 'organizations', controllers: {sessions: "organizations/sessions"}
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :job_applications, only: [:new, :create, :show]
-  resources :organizations
+
   root 'welcome#index'
 
   devise_scope :user do
     get 'sign_up', to: 'devise/registrations#new'
   end
+
 
 
   end
