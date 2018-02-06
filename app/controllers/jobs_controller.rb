ae272abc704ed5fa1db89f14957107a2a32075a0
tahbristol <tahbristol@gmail.com>
2018-02-05 19:35:14 -0500

    fixed org edit route and redirect

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,57 +1,55 @@
 class JobsController < ApplicationController
+ def index
+  @jobs = if params[:organization_id]
+           Job.find(params(:organization_id)).jobs
+          else
+           Job.all
+         end
+ end
 
-  def index
-    if params[:organization_id]
-      @jobs = Job.find(params(:organization_id)).jobs
-    else
-    @jobs = Job.all
-   end
+ def new
+  # binding.pry
+  @org = Organization.find(params[:organization_id])
+  @job = @org.jobs.build
+ end
+
+ def create
+  @org = Organization.find(params[:organization_id])
+  @job = @org.jobs.build(job_params)
+  @job.save
+  redirect_to organization_jobs_path(@org)
+ end
+
+ def show
+  @job = Job.find(params[:id])
+  @org = @job.organization
+ end
+
+ def edit
+  @job = Job.find(params[:id])
+ end
+
+ def update
+  @job = Job.find(params[:id])
+  @job.update(job_params)
+  redirect_to job_path(@job)
+ end
+
+ def apply
+  if current_user
+   @job = Job.find(params[:id])
+   current_user.jobs_applied << @job
+   current_user.save
+   redirect_to current_user
+  else
+   session[:viewed_job] = params[:id]
+   redirect_to new_user_session_path
   end
+ end
 
-  def new
-    #binding.pry
-    @org = Organization.find(params[:organization_id])
-    @job = @org.jobs.build
-  end
+ private
 
-  def create
-    binding.pry
-    @org = Organization.find(params[:organization_id])
-    @job = @org.jobs.build(job_params)
-    @job.save
-    redirect_to organization_jobs_path(@org)
-  end
-
-  def show
-    @job = Job.find(params[:id])
-    @org = @job.organization
-
-  end
-
-  def edit
-    @job = Job.find(params[:id])
-  end
-
-  def update
-    @job = Job.find(params[:id])
-    @job.update(job_params)
-    redirect_to job_path(@job)
-  end
-
-  def apply
-    if current_user
-      @job = Job.find(params[:id])
-      current_user.jobs_applied << @job
-      current_user.save
-      redirect_to current_user
-    else
-      session[:viewed_job] = params[:id]
-      redirect_to new_user_session_path
-    end
-  end
-
-  private
-  def job_params
-    params.require(:job).permit(:title, :location, :description, :level, :salary, :organization_id)
-  end
+ def job_params
+  params.require(:job).permit(:title, :location, :description, :level, :salary, :organization_id)
+ end
 end
--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,34 +1,33 @@
 class OrganizationsController < ApplicationController
+ def index; end
 
-  def index
-  end
+ def show
+  @org = Organization.find(params[:id])
+  @org_profile = @org.org_profile
+  # binding.pry
+ end
 
-  def show
-    @org = Organization.find(params[:id])
-    @org_profile = @org.org_profile
-    #binding.pry
-  end
+ def jobs
+  @org = current_organization
+  # binding.pry
+  @jobs = current_organization.jobs
+ end
 
-  def jobs
-    @org = current_organization
-    #binding.pry
-    @jobs = current_organization.jobs
-  end
+ def edit
+  @org = Organization.find(params[:id])
+ end
 
-  def edit
-    @org = Organization.find(params[:id])
-  end
+ def update
+  @org = Organization.find(params[:id])
+  @org.update(org_params)
+	binding.pry
+  redirect_to organization_path(@org)
+	#this redirects to root without image. Need to fix.
+ end
 
-  def update
-    @org = Organization.find(params[:id])
-    @org.update(org_params)
-    redirect_to organization_path(@org)
-  end
+ private
 
-  private
-
-  def org_params
-    params.require(:organization).permit(:name, :location, :email)
-  end
-
+ def org_params
+  params.require(:organization).permit(:name, :location, :email)
+ end
 end
